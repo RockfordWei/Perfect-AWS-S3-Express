d137f856691f3ee1a1775992e8929b81ceb9e443
RockfordWei <rocky@perfect.org>
2017-12-28 10:34:34 -0500

    Upgrading to Perfect 3

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:3.1
-
 import PackageDescription
+import Foundation
+
+let url: String
+if let cache = getenv("URL_PERFECT"), let local = String(validatingUTF8: cache) {
+  url = "\(local)/Perfect-CURL"
+} else {
+  url = "https://github.com/PerfectlySoft/Perfect-CURL.git"
+}
 
 let package = Package(
     name: "PerfectAWS",
     dependencies: [
-      .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2)
+      .Package(url: url, majorVersion: 3)
     ]
 )
